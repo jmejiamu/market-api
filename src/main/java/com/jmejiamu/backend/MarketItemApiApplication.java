39b7dddadd8b8eb7e8865f0be049d8eae5b64e3c
Jose <jmejiamu@gmail.com>
2021-01-17 20:13:17 -0800

    'basicjavaapi'

---
+++ src/main/java/com/jmejiamu/backend/MarketItemApiApplication.java
@@ -0,0 +1,13 @@
+package com.jmejiamu.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketItemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketItemApiApplication.class, args);
+	}
+
+}
