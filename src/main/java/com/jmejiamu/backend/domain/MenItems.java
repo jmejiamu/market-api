42f6a7bcb6264fd9d86e2ffc7404a8e9b29f0d3a
Jose <jmejiamu@gmail.com>
2021-01-18 22:28:51 -0800

    'added-menitem-table'

---
+++ src/main/java/com/jmejiamu/backend/domain/MenItems.java
@@ -0,0 +1,51 @@
+package com.jmejiamu.backend.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class MenItems {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String itemName;
+	private String itemDescrition;
+	private String itemUrl;
+	
+	public MenItems() {}
+
+	public MenItems(String itemName, String itemDescrition, String itemUrl) {
+		super();
+		this.itemName = itemName;
+		this.itemDescrition = itemDescrition;
+		this.itemUrl = itemUrl;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemDescrition() {
+		return itemDescrition;
+	}
+
+	public void setItemDescrition(String itemDescrition) {
+		this.itemDescrition = itemDescrition;
+	}
+
+	public String getItemUrl() {
+		return itemUrl;
+	}
+
+	public void setItemUrl(String itemUrl) {
+		this.itemUrl = itemUrl;
+	}
+	
+
+}
