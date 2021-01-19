58aa4e0e9e94b8d33c3b5e56dc4d497273a4e27b
Jose <jmejiamu@gmail.com>
2021-01-19 00:18:28 -0800

    'added-tested-data-to-DB'

--- src/main/java/com/jmejiamu/backend/MarketItemApiApplication.java
+++ src/main/java/com/jmejiamu/backend/MarketItemApiApplication.java
@@ -1,13 +1,31 @@
 package com.jmejiamu.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.jmejiamu.backend.domain.MenItemRepository;
+import com.jmejiamu.backend.domain.MenItems;
 
 @SpringBootApplication
 public class MarketItemApiApplication {
 
+	@Autowired
+	private MenItemRepository repository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MarketItemApiApplication.class, args);
+
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			MenItems men = new MenItems("Test description", "Test Name", "test URL");
+			repository.save(men);
+		};
 	}
 
 }
---
+++ src/main/java/com/jmejiamu/backend/domain/MenItemRepository.java
@@ -0,0 +1,9 @@
+package com.jmejiamu.backend.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MenItemRepository extends CrudRepository<MenItems, Long> {
+	
+}
