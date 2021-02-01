f0adaea21bd8e290557ffe96dabe9344bec56512
Jose <jmejiamu@gmail.com>
2021-02-01 00:33:20 -0800

    'allow-corns'

--- src/main/java/com/jmejiamu/backend/domain/MenItemRepository.java
+++ src/main/java/com/jmejiamu/backend/domain/MenItemRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin( origins =  "http://localhost:3000")
 @Repository
 public interface MenItemRepository extends CrudRepository<MenItems, Long> {
 	
--- src/main/java/com/jmejiamu/backend/domain/WomenItemRepository.java
+++ src/main/java/com/jmejiamu/backend/domain/WomenItemRepository.java
@@ -2,7 +2,9 @@ package com.jmejiamu.backend.domain;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin( origins =  "http://localhost:3000")
 @Repository
 public interface WomenItemRepository extends CrudRepository<WomenItem, Long>{
 
