5abb1300f2d13032c94874cdbf3b29a5477fdca0
Jose <jmejiamu@gmail.com>
2021-03-13 18:22:36 -0800

    'added-popular-end-point'

--- src/main/java/com/jmejiamu/backend/domain/Popular.java
+++ src/main/java/com/jmejiamu/backend/domain/Popular.java
@@ -1,5 +1,61 @@
 package com.jmejiamu.backend.domain;
 
-public class Popular {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+/*
+ * This entity creates an pupar table
+ */
+
+@Entity
+public class Popular {
+	/*
+	 *  This generate an auto increment ID
+	 */
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String itemName;
+	private String itemDescription;
+	private String itemUrl;
+	
+	public Popular() {
+		
+	}
+
+	public Popular(String itemName, String itemDescription, String itemUrl) {
+		super();
+		this.itemName = itemName;
+		this.itemDescription = itemDescription;
+		this.itemUrl = itemUrl;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemDescription() {
+		return itemDescription;
+	}
+
+	public void setItemDescription(String itemDescription) {
+		this.itemDescription = itemDescription;
+	}
+
+	public String getItemUrl() {
+		return itemUrl;
+	}
+
+	public void setItemUrl(String itemUrl) {
+		this.itemUrl = itemUrl;
+	}
+	
+	
 }
