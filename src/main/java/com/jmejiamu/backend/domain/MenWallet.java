052e28d58df255497c943feae8e24fced4c2e54e
Jose <jmejiamu@gmail.com>
2021-03-27 23:16:01 -0700

    'added-menwallet-entity'

---
+++ src/main/java/com/jmejiamu/backend/domain/MenWallet.java
@@ -0,0 +1,59 @@
+package com.jmejiamu.backend.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/*
+ * This entity creates an MenWallet table
+ */
+
+@Entity
+public class MenWallet {
+	/*
+	 * This generate an auto increment ID
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String itemName;
+	private String itemDescription;
+	private String itemUrl;
+	
+	public MenWallet() {}
+
+	public MenWallet(String itemName, String itemDescription, String itemUrl) {
+		super();
+		this.itemName = itemName;
+		this.itemDescription = itemDescription;
+		this.itemUrl = itemUrl;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemDescription() {
+		return itemDescription;
+	}
+
+	public void setItemDescription(String itemDescription) {
+		this.itemDescription = itemDescription;
+	}
+
+	public String getItemUrl() {
+		return itemUrl;
+	}
+
+	public void setItemUrl(String itemUrl) {
+		this.itemUrl = itemUrl;
+	}
+	
+	
+	
+}
