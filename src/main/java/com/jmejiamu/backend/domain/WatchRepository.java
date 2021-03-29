85259d89a9350ac6e2f594e3cba760ee5f8c229b
Jose <jmejiamu@gmail.com>
2021-03-28 22:33:38 -0700

    'configure-corns'

--- src/main/java/com/jmejiamu/backend/domain/WatchRepository.java
+++ src/main/java/com/jmejiamu/backend/domain/WatchRepository.java
@@ -1,7 +1,11 @@
 package com.jmejiamu.backend.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:3000")
+@Repository
 public interface WatchRepository extends CrudRepository<Watch, Long> {
 
 }
