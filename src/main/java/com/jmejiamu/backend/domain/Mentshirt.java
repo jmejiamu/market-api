469476e02fa3314ec7c2da6720a824b6070896a2
Jose <jmejiamu@gmail.com>
2021-04-19 17:52:35 -0700

    'added-the-pojo-for-mentshirt'

--- src/main/java/com/jmejiamu/backend/domain/Mentshirt.java
+++ src/main/java/com/jmejiamu/backend/domain/Mentshirt.java
@@ -1,5 +1,58 @@
 package com.jmejiamu.backend.domain;
 
-public class Mentshirt {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+/*
+ * This entity creates an Item Table
+ */
+
+@Entity
+public class Mentshirt {
+	/*
+	 * This generate an auto increment ID
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String itemName;
+	private String itemDescript;
+	private String itemUrl;
+	
+	public Mentshirt() {}
+
+	public Mentshirt(String itemName, String itemDescript, String itemUrl) {
+		super();
+		this.itemName = itemName;
+		this.itemDescript = itemDescript;
+		this.itemUrl = itemUrl;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemDescript() {
+		return itemDescript;
+	}
+
+	public void setItemDescript(String itemDescript) {
+		this.itemDescript = itemDescript;
+	}
+
+	public String getItemUrl() {
+		return itemUrl;
+	}
+
+	public void setItemUrl(String itemUrl) {
+		this.itemUrl = itemUrl;
+	}
+	
+	
 }
